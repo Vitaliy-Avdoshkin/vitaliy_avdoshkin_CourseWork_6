2c2062fd34fd75f74cfb7a3795b24fc0542eb8c1
AKVA <wioswa@gmail.com>
2024-12-16 08:32:41 +0300

    CW_6 django-project INIT

--- config/urls.py
+++ config/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("mailing.urls", namespace="mailing")),
-    path("contacts/", include("mailing.urls", namespace="mailing")),
+    path('admin/', admin.site.urls),
+    path('', include('mailing.urls', namespace='mailing')),
+
 ]
--- mailing/views.py
+++ mailing/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 
-
 def base(request):
 
     return render(request, "base.html")
+
